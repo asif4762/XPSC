a6a02d613d01af6520def7d0cde272d5379c49ff
asif4762 <zasif855@gmail.com>
2024-10-30 20:25:20 +0600

    indian_Summer

---
+++ week1/Topic Wise/Indian_Summer.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B,x.E
+#define fast() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pii pair<int,int>
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    map<pair<string,string>,int> mp;
+    for(int i = 0; i < n; i++)
+    {
+        string s;
+        string t;
+        cin >> s >> t;
+        mp[{s,t}] = 0;
+    }
+    
+    cout << mp.size() << endl;
+}
+
+int32_t main ()
+{
+    fast();
+    int t = 1;
+    // cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
