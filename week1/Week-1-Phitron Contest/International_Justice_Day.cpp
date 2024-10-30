8545e6f64c1ffc33b50c0e6a39838aa4ce25fa76
asif4762 <zasif855@gmail.com>
2024-10-30 19:13:29 +0600

    International_Justice_Day

---
+++ week1/Week-1-Phitron Contest/International_Justice_Day.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B,x.E
+#define fast() ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0)
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define pii pair<int,int>
+using namespace std;
+
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+
+    if(x < y)
+    cout << "NO" << endl;
+
+    else cout << "YES" << endl;
+}
+
+int32_t main ()
+{
+    fast();
+    int t = 1;
+    // cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
