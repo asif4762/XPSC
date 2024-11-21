8dbb6450e92c78a51cc6bc8ae1d6d6d87533051b
test <test>
2024-11-21 10:58:38 +0600

    update

--- week4/TopicWise/A-Segment-with-small-sum.cpp
+++ week4/TopicWise/A-Segment-with-small-sum.cpp
@@ -26,19 +26,17 @@ void ASIF()
     ll n, k;
     cin >> n >> k;
     vector<ll> v(n);
-    for(auto &it : v)
+    for(auto &it : v){
         cin >> it;
-    ll l = 0, r = 0, sum = 0, ans = 0;
-    while(r < n)
-    {
-        sum += v[r];
-        if(sum <= k)
-        {
-            ans = max(ans, r - l + 1);
+    }
+    ll l=0,r=0,sum=0,ans=0;
+    while(r<n){
+        sum+=v[r];
+        if(sum<=k){
+            ans=max(ans,r-l+1);
         }
-        else
-        {
-            sum -= v[l];
+        else{
+            sum-=v[l];
             l++;
         }
         r++;
