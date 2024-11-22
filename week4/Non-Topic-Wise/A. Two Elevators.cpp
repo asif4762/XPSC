acbf5b8317edcbe850f6f407a27acfefe63a14a9
test <test>
2024-11-22 15:58:44 +0600

    A. Two Elevators

---
+++ week4/Non-Topic-Wise/A. Two Elevators.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define mnso cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    int ans1=a-1, ans2=abs(b-c)+(c-1);
+    
+    if(ans1<ans2){
+        cout<<1<<endl;
+    }
+    else if(ans2<ans1){
+        cout<<2<<endl;
+    }
+    else{
+        cout<<3<<endl;
+    }
+}
+
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
