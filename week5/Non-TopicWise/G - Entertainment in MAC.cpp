fa18a0ff194a6dace855d9ebb690cf68263d9463
test <test>
2024-12-01 21:18:11 +0600

    G-Entertainment

---
+++ week5/Non-TopicWise/G - Entertainment in MAC.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n;
+    cin>>n;
+    string s,tmp;
+    cin>>s;
+    tmp=s;
+    reverse(all(tmp));
+    if(s<=tmp){
+        cout<<s<<endl;
+    }
+    else{
+        cout<<tmp<<s<<endl;
+    }
+}
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
