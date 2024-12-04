3fa30e008bb28d8e333f80751e88cebfc3fdb811
test <test>
2024-12-05 02:32:23 +0600

    D - Permutation Swap

---
+++ week6/Non-Topicwise/D - Permutation Swap.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<pair<int,int>>vp;
+    for(int i=0;i<n;i++){
+        vp.push_back({v[i],abs(i+1-v[i])});
+    }
+    int ans=0;
+    for(const auto[x,y]:vp){
+        if(y!=0){
+            ans=gcd(ans,y);
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
