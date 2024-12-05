dce024c01c2ba58a0da36510eefe5162745b8feb
test <test>
2024-12-05 16:12:29 +0600

    F - Good Prefixes

---
+++ week6/Non-Topicwise/F - Good Prefixes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    int mx=0,ans=0,sum=0;
+    for(int i=0;i<n;i++){
+        sum+=v[i];
+        mx=max(mx,v[i]);
+        if(sum-mx==mx){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
