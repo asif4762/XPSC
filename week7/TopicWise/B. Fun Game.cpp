2fcddd4577f3e4c1da176c8ad9922070046869ff
test <test>
2024-12-10 00:43:31 +0600

    B. Fun Game

---
+++ week7/TopicWise/B. Fun Game.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n;
+    cin >> n;
+    string s1,s2;
+    cin >> s1 >> s2;
+    if(s1 == s2){
+    	yes;
+    	return;
+    }
+    int pos = -1;
+    for(int i = 0; i < n; i++){
+    	if(s1[i]=='1'){
+    		pos=i;
+    		break;
+    	}
+    }
+    if(pos != -1){
+    	for(int i = 0; i < n; i++){
+    		if(s1[i]!=s2[i]){
+    			if(pos > i){
+    				no;
+    				return;
+    			}
+    		}
+    	}
+    	yes;
+    }
+    else{
+    	no;
+    }
+}
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
