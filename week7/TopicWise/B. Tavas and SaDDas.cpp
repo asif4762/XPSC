33f86c80f531c242cbef011eac183fbb145db465
test <test>
2024-12-11 01:15:56 +0600

    B. Tavas and SaDDas

---
+++ week7/TopicWise/B. Tavas and SaDDas.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    int ans = 0;
+    for(int i = 1; i < n; i++){
+        ans += (1LL << i);
+    }
+    for(int i = 0; i < n; i++){
+        if(s[i] == '7'){
+            ans += (1LL << (n - i - 1));
+        }
+    }
+    cout << ans + 1 << endl;
+}
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
