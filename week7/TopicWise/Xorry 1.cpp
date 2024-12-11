21c0d207d4c8d00d057e2d94da93b3a8b3978293
test <test>
2024-12-12 00:26:12 +0600

    Xorry 1

---
+++ week7/TopicWise/Xorry 1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n;
+    cin >> n;
+    int cnt=0;
+    int tmp1 = n;
+    while(n > 1){
+        n = n/2;
+        cnt++;
+    }
+    int tmp2 = (1LL << cnt);
+    cout << tmp1 - tmp2 << " " << tmp2 << endl;
+}
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
