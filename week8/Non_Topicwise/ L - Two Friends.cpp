55835266451bccfdcc2311c22d8c7e00f86d4f0a
test <test>
2024-12-20 00:29:19 +0600

    L - Two Friends

---
+++ week8/Non_Topicwise/ L - Two Friends.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n+1);
+    for(int i = 1; i <= n; i++){
+        cin >> v[i];
+    }
+    for(int i = 1; i <= n; i++){
+        int tmp = v[i];
+        if(v[tmp] == i){
+            cout << 2 << endl;
+            return;
+        }
+    }
+    cout << 3 << endl;
+}
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
