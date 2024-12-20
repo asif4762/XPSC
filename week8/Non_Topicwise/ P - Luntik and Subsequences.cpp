5a069448de9e604379468fed32e2f289ed60eb43
test <test>
2024-12-21 00:30:42 +0600

    P - Luntik and Subsequences

---
+++ week8/Non_Topicwise/ P - Luntik and Subsequences.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define B begin()
+#define E end()
+#define ll long long
+#define all(x) x.B, x.E
+#define rall(x) x.rbegin(), x.rend()
+#define int long long
+#define pii pair<ll, ll>
+#define mod 1000000007
+#define inf 1e18
+#define endl "\n"
+#define syes cout << "Yes" << endl
+#define sno cout << "No" << endl
+#define minus_one cout << -1 << endl
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define fastForwardRead()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+void ASIF()
+{
+    int n, cnt_zero = 0, cnt_one = 0;
+    cin >> n;
+    vector<int>v(n);
+    for(int i = 0; i < n; i++){
+        cin >> v[i];
+        if(v[i] == 0){
+            cnt_zero++;
+        }
+        if(v[i]==1){
+            cnt_one++;
+        }
+    }
+    cout << (ll)cnt_one * (1LL << cnt_zero) << endl;
+}
+
+int32_t main()
+{
+    fastForwardRead();
+    int t = 1;
+    cin >> t;
+    while (t--)
+        ASIF();
+    return 0;
+}
